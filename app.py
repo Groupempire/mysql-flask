11780adff6d4f8365beb9d30706bbc7036b77011
Harish Kumar bairagi <120214941+Groupempire@users.noreply.github.com>
2023-06-09 00:54:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('cryptoo.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        number = request.form['number']
+
+        db = mysql.connector.connect(
+            host='sql12.freesqldatabase.com',
+            user='sql12624615',
+            password='1HzdZwzDI7',
+            database='sql12624615'
+        )
+
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO solution (name, number) VALUES (%s, %s)", (name, number))
+        db.commit()
+        cursor.close()
+        db.close()
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3306)
